4a1c54974a1831a00f7fec29d8bb6d1becf603e6
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2025-06-26 11:15:29 +0530

    Create basic.py
    Day First

---
+++ basic.py
@@ -0,0 +1,13 @@
+# int float str
+
+# int - integer -- -veinfinity -1,0,1, ..... +Infinity
+# float - decimal -- -0.1,1.3,2.3
+# # str - characters - "a",'a', "bus ", "I am Rushi", '@'
+
+# ctrl + A = select all
+# ctrl + Z = undo
+# ctrl + Y = redo
+# ctrl + C = copy
+# ctrl + V = paste
+# ctrl + X = cut
+# ctrl + / = comment
