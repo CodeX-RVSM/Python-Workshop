602a1aca32e1d27f91d5594d57b519f0788ef8fd
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-10 14:26:37 +0530

    Comment out replacement logic in replace.py
    Commented out a block of code that replaces 3 with 6 in a 2D list.

--- replace.py
+++ replace.py
@@ -12,3 +12,11 @@ for row in y:
   x.append(new_row)
 
 print(x)
+
+
+# x=[[1,2,3],[4,5,3],[7,8,9]]
+# for i in range (len(x)):
+#   for j in range (len(x[i])):
+#     if x[i][j]==3:
+#       x[i][j]=6
+# print(x)
