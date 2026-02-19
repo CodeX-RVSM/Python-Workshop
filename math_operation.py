f95218b9682a24feca0156b9ef0a6575b8f6e071
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-19 14:04:34 +0530

    Create math_operation.py

---
+++ math_operation.py
@@ -0,0 +1,23 @@
+import math
+print(abs(-90))
+print(pow(2,3))
+print(round(234.3545345,1))
+print(min(23,36,22))
+print(max(22,45,23,31))
+print(math.sqrt(2))
+print(sum([1,2,3,4]))
+print(math.ceil(2.7))
+print(math.floor(2.7))
+print(math.factorial(5))
+print(math.gcd(6))
+print(math.sin(5))
+print(math.tan(5))
+print(math.pi)
+
+#area of circle 
+# r=3
+print("Area :",math.pi*pow(3,2))
+# area=math.pi*r**2
+# print(area)
+
+print("cir :",2*math.pi*3)
