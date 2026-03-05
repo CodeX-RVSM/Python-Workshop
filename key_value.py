1e092ec641e310356dfddb54098b24a2bd9587bf
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-03-05 23:31:13 +0530

    Create key_value.py

---
+++ key_value.py
@@ -0,0 +1,12 @@
+# Key = value
+def fullname(firstname,lastname):
+  print("my full name is ",firstname,lastname)
+fullname(lastname="kadu",firstname="ram")
+# user in
+
+num=input("enter your firstname");
+num1=input("enter your lastname");
+fullname(num,num1)
+def fullname(firstname,lastname):
+  print("my full name is ",firstname,lastname)
+fullname(lastname=num1,firstname=num)
